083f9d801e594fb1a8676d64c002dc95294a783c
zahra <zahra.st7373@gmail.com>
2023-11-10 21:04:53 +0330

    fix bug

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, GET } from "../Decorators/decorator.router";
 import { NextFunction, Request, Response } from 'express';
 
-@Controller('/user')
+@Controller()
 export   class HomeController{
-    @GET()
+    @GET('/users')
     GetHoeInfo(req:Request,res:Response , next:NextFunction){
         try {
-        return res.status(200).send("user")    
+        return res.send("users")    
         } catch (error) {
     
             next(error)
