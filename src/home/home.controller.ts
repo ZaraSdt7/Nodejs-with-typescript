285e5099f3667019cd2a58502cf14e682b5577ad
zahra <zahra.st7373@gmail.com>
2023-11-10 21:10:23 +0330

    .

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, GET } from "../Decorators/decorator.router";
 import { NextFunction, Request, Response } from 'express';
 
-@Controller()
+@Controller("/users")
 export   class HomeController{
-    @GET('/users')
+    @GET()
     GetHoeInfo(req:Request,res:Response , next:NextFunction){
         try {
         return res.send("users")    
